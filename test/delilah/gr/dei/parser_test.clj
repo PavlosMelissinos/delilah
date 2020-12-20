9d8e9369e2d1a4cc667521479c4a3de8204c38ef
Pavlos Melissinos <pmelissi@gmail.com>
2020-12-20 03:13:18 +0200

    Add unit tests

---
+++ test/delilah/gr/dei/parser_test.clj
@@ -0,0 +1,51 @@
+(ns delilah.gr.dei.parser-test
+  (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [clojure.tools.reader.edn :as edn]
+
+            [java-time :as t]
+
+            [delilah :as d]
+            [delilah.gr.dei.parser :as sut]))
+
+(deftest test-format-date
+  (let [datestr "2020-02-17"]
+    (is (= (-> (t/local-date datestr) sut/format-date)
+           datestr))))
+
+(deftest test-customer-info
+  (let [fragment [{:type :element, :attrs {:selected "selected", :value "0123456789"}, :tag :option, :content ["246813579 - Φούφουτος Ιωάννης - Στου διαόλου τη μάνα 666"]}]]
+    (is (= [{:description   "246813579 - Φούφουτος Ιωάννης - Στου διαόλου τη μάνα 666",
+             :selected      "selected",
+             :customer-code "0123456789"}]
+           (sut/customer-info fragment)))))
+
+(deftest test-active-customer-code
+  (let [fragment [{:type :element, :attrs {:selected "selected", :value "0123456789"}, :tag :option, :content ["246813579 - Φούφουτος Ιωάννης - Στου διαόλου τη μάνα 666"]}]]
+    (is (= "0123456789"
+           (sut/active-customer-code fragment)))))
+
+(deftest test-property-info
+  (let [fragment [{:type :element, :attrs {:id "ctl00_ctl00_Site_Main_Main_CustomerCodeDetails_fvCustomerCodeDetails_CustomerName_value"}, :tag :span, :content ["ΦΟΥΦΟΥΤΟΣ ΙΩΑΝΝΗΣ"]}]]
+    (is (= "ΦΟΥΦΟΥΤΟΣ ΙΩΑΝΝΗΣ"
+           (sut/property-info fragment))))
+  (let [fragment [{:type :element, :attrs {:id "ctl00_ctl00_Site_Main_Main_CustomerCodeDetails_fvCustomerCodeDetails_CustomerStreet_value"}, :tag :span, :content ["ΣΤΟΥ ΔΙΑΟΛΟΥ ΤΗ ΜΑΝΑ"]}]]
+    (is (= "ΣΤΟΥ ΔΙΑΟΛΟΥ ΤΗ ΜΑΝΑ"
+           (sut/property-info fragment))))
+  (let [fragment [{:type :element, :attrs {:id "ctl00_ctl00_Site_Main_Main_CustomerCodeDetails_fvCustomerCodeDetails_CustomerStreetNumber_value"}, :tag :span, :content ["666"]}]]
+    (is (= "666"
+           (sut/property-info fragment))))
+  (let [fragment [{:type :element, :attrs {:id "ctl00_ctl00_Site_Main_Main_CustomerCodeDetails_fvCustomerCodeDetails_CustomerCity_value"}, :tag :span, :content ["ΠΟΛΙΣ"]}]]
+    (is (= "ΠΟΛΙΣ"
+           (sut/property-info fragment))))
+  (let [fragment [{:type :element, :attrs {:id "ctl00_ctl00_Site_Main_Main_CustomerCodeDetails_fvCustomerCodeDetails_CustomerCode_value"}, :tag :span, :content ["246813579"]}]]
+    (is (= "246813579"
+           (sut/property-info fragment)))))
+
+(deftest test-bill
+  (let [fragment {:type :element, :attrs {:target "_blank", :href "UserBill.aspx?FileName=name-of-file.pdf", :title "Έκδοση 18.12.2019"}, :tag :a, :content ["\r\n                                    Έκδοση 18.12.2019\r\n                                "]}]
+    (is (= {:bill-date "2019-12-18",
+            :pdf-url
+            "https://www.dei.gr/EBill/UserBill.aspx?FileName=name-of-file.pdf"}
+           (-> (sut/bill fragment)
+               (update :bill-date #(t/format %)))))))
