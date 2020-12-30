f179976c8289789d9876b3665388997ef5cf7b37
Pavlos Melissinos <pmelissi@gmail.com>
2020-12-30 19:43:37 +0200

    Finalize mail parser logic and add tests

--- components/mailer/src/delilah/gr/dei/mailer/parser.clj
+++ components/mailer/src/delilah/gr/dei/mailer/parser.clj
@@ -2,7 +2,8 @@
   (:require [clojure.string :as str]
             [hickory.select :as hs]
             [hickory-css-selectors :as hcs]
-            [delilah.common.parser :as cparser]))
+            [delilah.common.parser :as cparser]
+            [java-time :as t]))
 
 (def codes
   {:contract-account "Contract Account"
@@ -27,16 +28,35 @@
        rest
        str/join))
 
-(defn dom [email]
-  (-> email :body :body cparser/parse))
+(defn parse2019 [dom]
+  (let [codes         [:contract-account :e-payment-code :amount :expiration-date]
+        fragments-raw (hs/select (hcs/parse-css-selector ".EBill td b") dom)
+        fragments     (->> (take 4 fragments-raw)
+                           (map :content)
+                           (map first)
+                           (zipmap codes)
+                           #_(filter valid-field?)
+                           #_(map (juxt field-key field-value))
+                           #_(into {}))]
+    fragments))
 
-(defn parse [dom]
-  (let [fragments (->> (hs/select (hcs/parse-css-selector ".wrapper td p") dom)
-                       (map :content)
-                       (filter valid-field?)
-                       (map (juxt field-key field-value))
-                       (into {}))]
+(defn parse2020 [dom]
+  (let [fragments-raw (hs/select (hcs/parse-css-selector ".wrapper td p") dom)
+        fragments     (->> (map :content fragments-raw)
+                           (filter valid-field?)
+                           (map (juxt field-key field-value))
+                           (into {}))]
     (reduce-kv (fn [m k v]
                  (assoc m k (get fragments v)))
                {}
                codes)))
+
+(defn parse [{:keys [date-received] :as email}]
+  (log/info (str "Parsing email of " date-received))
+  (let [dom (-> email :body :body cparser/parse)]
+    (cond
+      (t/after? (t/instant date-received) (t/instant (t/zoned-date-time 2019 12 1)))
+      (parse2020 dom)
+
+      :else
+      (parse2019 dom))))
